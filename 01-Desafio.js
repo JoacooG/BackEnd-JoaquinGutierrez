779646945385dc3cef26a7cb89b1da7b337d8d7a
Joaquin Gutierrez <joacoo101@gmail.com>
2022-10-04 00:32:44 -0300

    desafio corregido

--- 01-Desafio.js
+++ 01-Desafio.js
@@ -1,26 +1,24 @@
 class User {
     name;
     lastname;
-    books;
-    pets;
+    books = []
+    pets = []
     constructor (name, lastname){
         this.name = name;
         this.lastname = lastname;
-        this.books = [];
-        this.pets = [];
     }
     getFullName(){
         console.log(`User: ${this.name} ${this.lastname}`);
     }
-    addPet(){
-        this.pets.push('Perro');
+    addPet(pet){
+        this.pets.push(pet);
     }
     countPets(){
         let total = this.pets.length
         console.log(total);
     }
-    addBook(){
-        const book = {name: 'The lord of the rings', author: 'JRR Tolkien'}
+    addBook(name, author){
+        const book = {name, author}
         this.books.push(book)
     }
     getBooksNames(){
@@ -30,7 +28,8 @@ class User {
 
 let user1 = new User('Lionel', 'Messi');
 user1.getFullName();
-user1.addPet();
+user1.addPet('Gato');
+user1.addPet('Perro')
 user1.countPets();
-user1.addBook();
+user1.addBook('The lord of the Rings', 'JRR Tolkien');
 user1.getBooksNames();
